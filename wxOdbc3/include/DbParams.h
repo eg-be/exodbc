9864528f336bf771f760b086869f56e6af4e0f1c
eg <23149233+eg-be@users.noreply.github.com>
2014-02-09 14:40:00 +0000

    First success: A unit-test that connects with success to a mysql-db.

---
+++ wxOdbc3/include/DbParams.h
@@ -0,0 +1,32 @@
+/*!
+ * \file DbParams.h
+ * \author Elias Gerber <egerber@gmx.net>
+ * \date 09.02.2014
+ * 
+ * Parameters to connect to different datasources
+ */ 
+
+#pragma once
+#ifndef DBPARAMS_H
+#define DBPARAMS_H
+
+// Same component headers
+// Other headers
+// System headers
+
+#define MYSQL_DSN			L"wxodbc3MySql"
+#define MYSQL_USER			L"wxodbc3"
+#define MYSQL_PASS			L"wxodbc3"
+
+// Forward declarations
+// --------------------
+
+// Structs
+// -------
+
+// Classes
+// -------
+
+
+
+#endif // DBPARAMS_H
--- wxOdbc3/test/DbTest.cpp
+++ wxOdbc3/test/DbTest.cpp
@@ -20,8 +20,10 @@
 #include "DbTest.h"
 
 // Same component headers
-// Other headers
+#include "DbParams.h"
+#include "db.h"
 
+// Other headers
 #include "cppunit/config/SourcePrefix.h"
 
 // Static consts
@@ -33,27 +35,50 @@
 // Destructor
 // -----------
 
-// Implementation
-// --------------
+// DbConnectInfTest
+// ----------------
+void DbConnectInfTest::testAllocHenv()
+{
+	// Allocating a Hevn will always work, no tests for users / pws are done atm
+	wxDbConnectInf DbConnectInf;
+	DbConnectInf.SetDsn(L"MyDSN");
+	DbConnectInf.SetUserID(L"MyUserName");
+	DbConnectInf.SetPassword(L"MyPassword");
+	DbConnectInf.SetDefaultDir(L"");	
+	CPPUNIT_ASSERT( DbConnectInf.AllocHenv() );
+	CPPUNIT_ASSERT( DbConnectInf.GetHenv() != 0 );
+
+	// Create using c'tor
+	wxDbConnectInf DbConnectInf2(NULL, L"MyDSN", L"UserId", L"Pass");
+	CPPUNIT_ASSERT( DbConnectInf.GetHenv() != 0 );
+}
+
+// DbTest
+// ------
 void DbTest::setUp()
 {
-
+	// Create DbConnectInfs for various databases
+	m_pConnectInfMySql = new wxDbConnectInf(NULL, MYSQL_DSN, MYSQL_USER, MYSQL_PASS);
+	m_pDbMySql = new wxDb(m_pConnectInfMySql->GetHenv());
 }
 
 
 void DbTest::tearDown()
 {
-
+	delete m_pConnectInfMySql;
+	delete m_pDbMySql;
 }
 
-void DbTest::testFail()
-{
-	CPPUNIT_ASSERT( false );
-}
 
-void DbTest::testOk()
+void DbTest::testOpen()
 {
-	CPPUNIT_ASSERT( true );
+	// Open with failing on unsupported datatypes
+	CPPUNIT_ASSERT( m_pDbMySql->Open(m_pConnectInfMySql, true) );
+
+	// Try to open one with not only forward-cursorts
+	wxDb* pdbMySqlBC = new wxDb(m_pConnectInfMySql->GetHenv(), false);
+	CPPUNIT_ASSERT( pdbMySqlBC->Open(m_pConnectInfMySql, true));
+	delete pdbMySqlBC;
 }
 
 // Interfaces
