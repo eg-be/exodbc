13ab739989aca383d7169ed9794306c6e9650dd5
eg <23149233+eg-be@users.noreply.github.com>
2014-05-04 19:55:35 +0000

    better naming for tests.

--- wxOdbc3/wxOdbc3GoogleTest/DbTest.h
+++ wxOdbc3/wxOdbc3GoogleTest/DbTest.h
@@ -21,19 +21,19 @@ namespace wxOdbc3Test
 
 
 	INSTANTIATE_TEST_CASE_P(
-		Open_MySql_3_51,
+		MySql_3_51,
 		DbTest,
 		::testing::Values(	SOdbcInfo(MYSQL_3_51_DSN, MYSQL_USER, MYSQL_PASS, SOdbcInfo::forwardOnlyCursors),
 							SOdbcInfo(MYSQL_3_51_DSN, MYSQL_USER, MYSQL_PASS, SOdbcInfo::notForwardOnlyCursors)));
 
 	INSTANTIATE_TEST_CASE_P(
-		Open_MySql_5_2,
+		MySql_5_2,
 		DbTest,
 		::testing::Values(	SOdbcInfo(MYSQL_5_2_DSN, MYSQL_USER, MYSQL_PASS, SOdbcInfo::forwardOnlyCursors),
 		SOdbcInfo(MYSQL_5_2_DSN, MYSQL_USER, MYSQL_PASS, SOdbcInfo::notForwardOnlyCursors)));
 
 	INSTANTIATE_TEST_CASE_P(
-		Open_DB2,
+		IBM_DB2,
 		DbTest,
 		::testing::Values(	SOdbcInfo(DB2_DSN, DB2_USER, DB2_PASS, SOdbcInfo::forwardOnlyCursors),
 		SOdbcInfo(DB2_DSN, DB2_USER, DB2_PASS, SOdbcInfo::notForwardOnlyCursors)));
