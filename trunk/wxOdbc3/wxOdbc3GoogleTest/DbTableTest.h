d4fbd18d82fdcc5b24d9edf3c36e7895e043668b
eg <eg@7898c5a7-19f2-4356-953d-35c5c242a1bf>
2014-05-04 19:55:35 +0000

    better naming for tests.

--- trunk/wxOdbc3/wxOdbc3GoogleTest/DbTableTest.h
+++ trunk/wxOdbc3/wxOdbc3GoogleTest/DbTableTest.h
@@ -28,17 +28,17 @@ namespace wxOdbc3Test
 	};
 
 	INSTANTIATE_TEST_CASE_P(
-		ReadTypes_MySql_3_51,
+		MySql_3_51,
 		DbTableTest,
 		::testing::Values(	SOdbcInfo(MYSQL_3_51_DSN, MYSQL_USER, MYSQL_PASS, SOdbcInfo::forwardOnlyCursors)));
 
 	INSTANTIATE_TEST_CASE_P(
-		ReadTypes_MySql_5_2,
+		MySql_5_2,
 		DbTableTest,
 		::testing::Values(	SOdbcInfo(MYSQL_5_2_DSN, MYSQL_USER, MYSQL_PASS, SOdbcInfo::forwardOnlyCursors)));
 
 	INSTANTIATE_TEST_CASE_P(
-		ReadTypes_IBM_DB_2,
+		IBM_DB2,
 		DbTableTest,
 		::testing::Values(	SOdbcInfo(DB2_DSN, DB2_USER, DB2_PASS, SOdbcInfo::forwardOnlyCursors)));
 
