cd4a0f72122174bd26c9fec99ab8f6debc1a7e59
eg <23149233+eg-be@users.noreply.github.com>
2014-05-04 15:41:14 +0000

    Params printed in case of failure

--- wxOdbc3/wxOdbc3GoogleTest/DbTest.cpp
+++ wxOdbc3/wxOdbc3GoogleTest/DbTest.cpp
@@ -23,7 +23,6 @@ namespace wxOdbc3Test
 		SOdbcInfo odbcInfo = GetParam();
 		m_pConnectInf = new wxDbConnectInf(NULL, odbcInfo.m_dsn, odbcInfo.m_username, odbcInfo.m_password);
 		m_forwardOnlyCursors = (odbcInfo.m_cursorType == SOdbcInfo::forwardOnlyCursors);
-		int a = 3;
 	}
 
 	void DbTest::TearDown()
@@ -32,30 +31,11 @@ namespace wxOdbc3Test
 		m_pConnectInf = NULL;
 	}
 
-	//TEST_F(DbTest, OpenForwardOnly_MySql_3_51)
-	//{
-	//	OpenTest(MYSQL_3_51_DSN, MYSQL_USER, MYSQL_PASS, true);
-	//}
-
-	//TEST_F(DbTest, OpenNotForwardOnly_MySql_3_51)
-	//{
-	//	OpenTest(MYSQL_3_51_DSN, MYSQL_USER, MYSQL_PASS, false);
-	//}
-
-	//TEST_F(DbTest, OpenForwardOnly_DB2)
-	//{
-	//	OpenTest(DB2_DSN, DB2_USER, DB2_PASS, true);
-	//}
-
-	//TEST_F(DbTest, OpenNotForwardOnly_DB2)
-	//{
-	//	OpenTest(DB2_DSN, DB2_USER, DB2_PASS, false);
-	//}
 
 	TEST_P(DbTest, OpenTest)
 	{
 		HENV henv = m_pConnectInf->GetHenv();
-		ASSERT_TRUE(henv);
+		ASSERT_TRUE(henv  != 0);
 
 		wxDb* pDb = new wxDb(henv, m_forwardOnlyCursors);
 
@@ -69,32 +49,6 @@ namespace wxOdbc3Test
 		EXPECT_TRUE(pDb->Open(m_pConnectInf, true));
 		pDb->Close();
 		delete pDb;
-	}
-
-	void DbTest::OpenTest(const std::wstring dsn, const std::wstring user, const std::wstring pass, bool forwardOnlyCursors)
-	{
-		wxDbConnectInf* pConnectInf = new wxDbConnectInf(NULL, dsn, user, pass);
-		HENV henv = pConnectInf->GetHenv();
-		if(!henv)
-		{
-			delete pConnectInf;
-			ASSERT_TRUE(henv);
-		}
-
-		wxDb* pDb = new wxDb(henv, forwardOnlyCursors);
-
-		// Open without failing on unsupported datatypes
-		EXPECT_TRUE(pDb->Open(pConnectInf, false));
-		pDb->Close();
-		delete pDb;
-
-		// Open with failing on unsupported datatypes
-		pDb = new wxDb(henv, forwardOnlyCursors);
-		EXPECT_TRUE(pDb->Open(pConnectInf, true));
-		pDb->Close();
-
-		delete pDb;
-		delete pConnectInf;
 	}
 
 }
--- wxOdbc3/wxOdbc3GoogleTest/DbTest.h
+++ wxOdbc3/wxOdbc3GoogleTest/DbTest.h
@@ -3,10 +3,27 @@
 #include "MySqlParams.h"
 #include "Db2Params.h"
 
+#include <iostream>
+#include <windows.h>
+
 class wxDbConnectInf;
 
 namespace wxOdbc3Test
 {
+
+	std::string w2mb(const std::wstring& wstr, unsigned int codepage = CP_UTF8)
+	{
+		int size_needed = WideCharToMultiByte(codepage, 0, &wstr[0], (int) wstr.size(), NULL, 0, NULL, NULL);
+		if(size_needed == 0)
+			return std::string("");
+		std::string strTo(size_needed, 0);
+		if (WideCharToMultiByte(codepage, 0, wstr.c_str(), (int) wstr.size(), &strTo[0], size_needed, NULL, NULL) < 1)
+		{
+			return "Failed To Convert";
+		}
+		return strTo;
+		return "";
+	}
 
 	struct SOdbcInfo
 	{
@@ -28,19 +45,25 @@ namespace wxOdbc3Test
 		CursorType m_cursorType;
 	};
 
+	::std::ostream& operator<<(::std::ostream& os, const SOdbcInfo& oi) {
+		std::wstringstream wos;
+		wos << L"DSN: " << oi.m_dsn.c_str() 
+			<< L"; Username: " << oi.m_username.c_str()
+			<< L"; Password: " << oi.m_password.c_str() 
+			<< L"; Forward-Only Cursor: " << (oi.m_cursorType == SOdbcInfo::forwardOnlyCursors);
+		return os << w2mb(wos.str());
+	}
+
 	class DbTest : public ::testing::TestWithParam<SOdbcInfo>
 	{
 	protected:
 		virtual void SetUp();
 		virtual void TearDown();
 
-		//private:
-		void OpenTest(const std::wstring dsn, const std::wstring user, const std::wstring pass, bool forwardOnlyCursors);
-		void OpenTest(wxDbConnectInf* pConnectInf);
-
 		wxDbConnectInf* m_pConnectInf;
 		bool m_forwardOnlyCursors;
 	};
+
 
 	INSTANTIATE_TEST_CASE_P(
 		Open_MySql_3_51,
