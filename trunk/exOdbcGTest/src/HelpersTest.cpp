5ca9db70d780d613525d64a9bef6fb31f3c68e68
eg <eg@7898c5a7-19f2-4356-953d-35c5c242a1bf>
2015-03-28 16:04:06 +0000

    #88 IsEqual

--- trunk/exOdbcGTest/src/HelpersTest.cpp
+++ trunk/exOdbcGTest/src/HelpersTest.cpp
@@ -381,6 +381,41 @@ namespace exodbc
 		EXPECT_TRUE(memcmp(&num, &nullMem, sizeof(SQL_NUMERIC_STRUCT)) == 0);
 	}
 
+
+	TEST_F(StaticHelpersTest, IsTimeEqual)
+	{
+		SQL_TIME_STRUCT t1 = InitTime(13, 14, 15);
+		SQL_TIME_STRUCT t2 = InitTime(07, 14, 27);
+		SQL_TIME_STRUCT t3 = InitTime(13, 14, 15);
+		EXPECT_TRUE(IsTimeEqual(t1, t3));
+		EXPECT_FALSE(IsTimeEqual(t1, t2));
+		EXPECT_FALSE(IsTimeEqual(t3, t2));
+
+	}
+
+
+	TEST_F(StaticHelpersTest, IsDateEqual)
+	{
+		SQL_DATE_STRUCT d1 = InitDate(26, 01, 1983);
+		SQL_DATE_STRUCT d2 = InitDate(25, 04, 1983);
+		SQL_DATE_STRUCT d3 = InitDate(26, 01, 1983);
+		EXPECT_TRUE(IsDateEqual(d1, d3));
+		EXPECT_FALSE(IsDateEqual(d1, d2));
+		EXPECT_FALSE(IsDateEqual(d3, d2));
+	}
+
+
+	TEST_F(StaticHelpersTest, IsTimestampEqual)
+	{
+		SQL_TIMESTAMP_STRUCT ts1 = InitTimestamp(13, 14, 15, 123456789, 26, 1, 1983);
+		SQL_TIMESTAMP_STRUCT ts2 = InitTimestamp(13, 14, 15, 12345, 26, 1, 1983);
+		SQL_TIMESTAMP_STRUCT ts3 = InitTimestamp(13, 14, 15, 123456789, 26, 1, 1983);
+
+		EXPECT_TRUE(IsTimestampEqual(ts1, ts3));
+		EXPECT_FALSE(IsTimestampEqual(ts1, ts2));
+		EXPECT_FALSE(IsTimestampEqual(ts3, ts2));
+	}
+
 	// Interfaces
 	// ----------
 
