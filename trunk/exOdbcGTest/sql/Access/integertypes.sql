2328f40ed14d5c579e10a559d51544d1a94f57aa
eg <eg@7898c5a7-19f2-4356-953d-35c5c242a1bf>
2015-04-04 22:27:16 +0000

    more access scripts

--- trunk/exOdbcGTest/sql/Access/integertypes.sql
+++ trunk/exOdbcGTest/sql/Access/integertypes.sql
@@ -3,13 +3,14 @@ DROP TABLE integertypes;
 CREATE TABLE integertypes (
 		idintegertypes Long PRIMARY KEY,
 		tsmallint Integer,
-		tint Long
+		tint Long,
+		tbigint Long
 	);
 
-INSERT INTO integertypes (idintegertypes, tsmallint, tint) VALUES (1,-32768,NULL);
-INSERT INTO integertypes (idintegertypes, tsmallint, tint) VALUES (2,32767,NULL);
-INSERT INTO integertypes (idintegertypes, tsmallint, tint) VALUES (3,NULL,-2147483648);
-INSERT INTO integertypes (idintegertypes, tsmallint, tint) VALUES (4,NULL,2147483647);
-INSERT INTO integertypes (idintegertypes, tsmallint, tint) VALUES (5,NULL,NULL);
-INSERT INTO integertypes (idintegertypes, tsmallint, tint) VALUES (6,NULL,NULL);
-INSERT INTO integertypes (idintegertypes, tsmallint, tint) VALUES (7,-13,26);
+INSERT INTO integertypes (idintegertypes, tsmallint, tint) VALUES (1,-32768,NULL, NULL);
+INSERT INTO integertypes (idintegertypes, tsmallint, tint) VALUES (2,32767,NULL, NULL);
+INSERT INTO integertypes (idintegertypes, tsmallint, tint) VALUES (3,NULL,-2147483648, NULL);
+INSERT INTO integertypes (idintegertypes, tsmallint, tint) VALUES (4,NULL,2147483647, NULL);
+INSERT INTO integertypes (idintegertypes, tsmallint, tint) VALUES (5,NULL,NULL, NULL);
+INSERT INTO integertypes (idintegertypes, tsmallint, tint) VALUES (6,NULL,NULL, NULL);
+INSERT INTO integertypes (idintegertypes, tsmallint, tint) VALUES (7,-13,26, 10502);
