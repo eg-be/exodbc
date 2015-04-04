0011611a4af87d7880a774114b6449247f561150
eg <eg@7898c5a7-19f2-4356-953d-35c5c242a1bf>
2015-04-04 14:41:27 +0000

    Add more tables for mysql

--- trunk/exOdbcGTest/sql/MySql/datetypes_tmp.sql
+++ trunk/exOdbcGTest/sql/MySql/datetypes_tmp.sql
@@ -1,5 +1,5 @@
-DROP TABLE IF EXISTS `datetypes`;
-CREATE TABLE `datetypes` (
+DROP TABLE IF EXISTS `datetypes_tmp`;
+CREATE TABLE `datetypes_tmp` (
   `iddatetypes` int(11) NOT NULL,
   `tdate` date DEFAULT NULL,
   `ttime` time DEFAULT NULL,
