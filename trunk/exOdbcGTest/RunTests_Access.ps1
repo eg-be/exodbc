17977225e7e1b43c4ff7cc6a76c7f4b222e1c54e
eg <eg@7898c5a7-19f2-4356-953d-35c5c242a1bf>
2015-06-07 19:39:59 +0000

    Add a script for Access.

---
+++ trunk/exOdbcGTest/RunTests_Access.ps1
@@ -0,0 +1,28 @@
+#
+# RunTests_Access.ps1
+#
+
+param(
+	[Parameter(Mandatory=$true)]
+	[string]$Target,
+	[string]$Dsn="exAccess",
+	[string]$ConnectionString="Driver={Microsoft Access Driver (*.mdb)};Dbq=E:\exOdbc\exOdbcGTest\access\exodbc.mdb;Uid=Admin;Pwd=;",
+	[string]$LogLevel="--logLevelW",
+	[string]$filterDsn="*-Excel*:*TableTest.OpenAutoCheckPrivs*:*TableTest.*UnsupportedColumn*:*TableTest.*Numeric*:*DatabaseTest.ReadSchemas*:*DatabaseTest.ReadTablePrivileges*:*DatabaseTest.ReadTablePrimaryKeysInfo*:*wxCompatibilityTest*",
+	[string]$filterCs="*-Excel*:*DetectDbms*:*ListDataSources*:*TableTest.OpenAutoCheckPrivs*:*TableTest.*UnsupportedColumn*:*TableTest.*Numeric*:*DatabaseTest.ReadSchemas*:*DatabaseTest.ReadTablePrivileges*:*DatabaseTest.ReadTablePrimaryKeysInfo*:*wxCompatibilityTest*",
+	[string]$case="l"
+)
+
+$ScriptPath = Split-Path $MyInvocation.InvocationName
+$args = @()
+$args += ("-Target", $Target)
+$args += ("-Dsn", $Dsn)
+$args += ("-ConnectionString", """$ConnectionString""")
+$args += ("-LogLevel", $LogLevel)
+$args += ("-filterDsn", $filterDsn)
+$args += ("-filterCs", $filterCs)
+$args += ("-case", $case)
+
+$cmd = "$ScriptPath\RunTests.ps1"
+Write-Host $cmd
+Invoke-Expression "$cmd $args"
