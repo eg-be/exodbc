7c55abfeb54c6582be8f7c8ffa17bd868a8424ec
eg <eg@7898c5a7-19f2-4356-953d-35c5c242a1bf>
2015-06-14 18:33:00 +0000

    Add some untested helper to determine the sql c type from the BufferPtrVariant #152

--- trunk/exOdbc/include/ColumnBuffer.h
+++ trunk/exOdbc/include/ColumnBuffer.h
@@ -160,6 +160,9 @@ namespace exodbc
 	class EXODBCAPI ColumnBuffer
 	{
 	public:
+
+		static SQLSMALLINT GetSqlCType(const BufferPtrVariant& pBuffer, OdbcVersion version);
+
 		/*!
 		* \brief	Create a new ColumnBuffer that will allocate a corresponding buffer 
 		*			using the data type information from the passed ColumnInfo.
