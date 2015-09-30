67d9c2b7bebfd0294e42920fc98ebf10f43d97f1
eg <eg@7898c5a7-19f2-4356-953d-35c5c242a1bf>
2015-09-30 14:42:53 +0000

    delete wrong release 0.7.4

--- tags/exOdbc_0.7.4/exOdbcGTest/sql/MySql/floattypes.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS `floattypes`;
-CREATE TABLE `floattypes` (
-  `idfloattypes` int(11) NOT NULL,
-  `tdouble` double DEFAULT NULL,
-  `tfloat` float DEFAULT NULL,
-  PRIMARY KEY (`idfloattypes`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-INSERT INTO `floattypes` VALUES (1,NULL,0),(2,NULL,3.141),(3,NULL,-3.141),(4,0,NULL),(5,3.141592,NULL),(6,-3.141592,NULL);
--- tags/exOdbc_0.7.4/exOdbcGTest/sql/MySql/floattypes_tmp.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS `floattypes_tmp`;
-CREATE TABLE `floattypes_tmp` (
-  `idfloattypes` int(11) NOT NULL,
-  `tdouble` double DEFAULT NULL,
-  `tfloat` float DEFAULT NULL,
-  PRIMARY KEY (`idfloattypes`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
