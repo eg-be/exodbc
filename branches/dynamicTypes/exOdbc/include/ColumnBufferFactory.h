9e52873db9d140f7c0af7ce09f8cd3c827ca9109
eg <eg@7898c5a7-19f2-4356-953d-35c5c242a1bf>
2015-10-18 12:25:41 +0000

    Prepare for ColumnBufferFactory

---
+++ branches/dynamicTypes/exOdbc/include/ColumnBufferFactory.h
@@ -0,0 +1,58 @@
+/*!
+* \file ColumnBufferFactory.h
+* \author Elias Gerber <eg@elisium.ch>
+* \date 18.10.2015
+* \brief Header file for the CBufferType interface.
+* \copyright GNU Lesser General Public License Version 3
+*/
+
+#pragma once
+
+// Same component headers
+#include "exOdbc.h"
+#include "Exception.h"
+
+// Other headers
+// System headers
+
+// Forward declarations
+// --------------------
+
+namespace exodbc
+{
+	// Consts
+	// ------
+
+	// Structs
+	// -------
+
+	// Classes
+	// -------
+	class IColumnBuffer
+	{
+	public:
+	};
+
+	class ColumnBufferFactory
+	{
+		typedef std::function<std::shared_ptr<IColumnBuffer>(SQLSMALLINT)> BufferCreationFunc;
+		typedef std::map<SQLSMALLINT, BufferCreationFunc> BufferCreatorFuncsMap;
+
+	public:
+		static ColumnBufferFactory& Instance() throw();
+		~ColumnBufferFactory() {};
+
+	private:
+		ColumnBufferFactory() {};
+		ColumnBufferFactory(const ColumnBufferFactory& other) {};
+
+		std::shared_ptr<IColumnBuffer> CreateColumnBuffer(SQLSMALLINT sqlCBufferType) const;
+		void RegisterColumnBufferCreationFunc(SQLSMALLINT sqlCBufferType, BufferCreationFunc func);
+
+	private:
+
+		BufferCreatorFuncsMap m_creatorFuncs;
+	};
+
+
+} // namespace exodbc
