522385654bab0b13ea17537b987cc9da57ceed96
eg <eg@7898c5a7-19f2-4356-953d-35c5c242a1bf>
2015-11-22 18:03:20 +0000

    TEmplate for the RowDescriptor Handle, prepare SqlHandleTest

---
+++ branches/sqlCBuffer/exOdbcGTest/src/SqlHandleTest.cpp
@@ -0,0 +1,38 @@
+/*!
+* \file SqlHandleTest.cpp
+* \author Elias Gerber <eg@elisium.ch>
+* \date 22.11.2014
+* \copyright GNU Lesser General Public License Version 3
+*
+* [Brief CPP-file description]
+*/
+
+#include "stdafx.h"
+
+// Own header
+#include "SqlHandleTest.h"
+
+// Same component headers
+// Other headers
+
+
+// Debug
+#include "DebugNew.h"
+
+namespace exodbc
+{
+	// Static consts
+	// -------------
+
+	// Construction
+	// -------------
+
+	// Destructor
+	// -----------
+
+	// Implementation
+	// --------------
+
+
+
+} // namespace exodbc
