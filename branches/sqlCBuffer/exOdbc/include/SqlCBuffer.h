93b9b1279d024cd822cac113086e95ac12d3aa5b
eg <eg@7898c5a7-19f2-4356-953d-35c5c242a1bf>
2015-11-28 14:36:23 +0000

    theoretically this should not fit into the variant.

--- branches/sqlCBuffer/exOdbc/include/SqlCBuffer.h
+++ branches/sqlCBuffer/exOdbc/include/SqlCBuffer.h
@@ -421,14 +421,113 @@ namespace exodbc
 		SqlCPointerBuffer& operator=(const SqlCPointerBuffer& other) = default;
 		SqlCPointerBuffer(const SqlCPointerBuffer& other) = default;
 
+		virtual ~SqlCPointerBuffer()
+		{
+			// unbind selects, one by one, do not stop if one fails but try the next one
+			// (thats why this is not done using Unbind() )
+			std::set<ColumnBoundHandle>::iterator it = m_boundSelects.begin();
+			while (it != m_boundSelects.end())
+			{
+				ColumnBoundHandle bindInfo = *it;
+				try
+				{
+					if (bindInfo.m_pHStmt && bindInfo.m_pHStmt->IsAllocated())
+					{
+						it = UnbindSelect(bindInfo.m_columnNr, bindInfo.m_pHStmt);
+					}
+					else
+					{
+						LOG_ERROR(L"Unexpected ColumnBoundHandle bindInfo: Either m_pHStmt not set, or m_pHStmt not allocated.");
+					}
+				}
+				catch (const Exception& ex)
+				{
+					LOG_ERROR(boost::str(boost::wformat(L"Failed to unbind column %d from stmt-handle %d: %s") % bindInfo.m_columnNr %bindInfo.m_pHStmt->GetHandle() % ex.ToString()));
+					++it;
+				}
+			}
+		};
+
 		SQLSMALLINT GetSqlCType() const noexcept { return m_sqlCType; };
 		SQLLEN GetBufferLength() const noexcept { return m_bufferLength; };
+
+		void BindSelect(SQLUSMALLINT columnNr, ConstSqlStmtHandlePtr pHStmt)
+		{
+			exASSERT(columnNr >= 1);
+			exASSERT(pHStmt != NULL);
+			exASSERT(pHStmt->IsAllocated());
+			exASSERT(m_columnSize >= 0);
+			exASSERT(m_decimalDigits >= 0);
+			ColumnBoundHandle boundHandleInfo(pHStmt, columnNr);
+			exASSERT_MSG(m_boundSelects.find(boundHandleInfo) == m_boundSelects.end(), L"Already bound to passed hStmt and column for Select on this buffer");
+
+			if (m_columnSize > 0 || m_decimalDigits > 0)
+			{
+				SqlDescHandle hDesc(pHStmt, RowDescriptorType::ROW);
+				SetDescriptionField(hDesc.GetHandle(), columnNr, SQL_DESC_TYPE, (SQLPOINTER)SQL_C_NUMERIC);
+				SetDescriptionField(hDesc.GetHandle(), columnNr, SQL_DESC_PRECISION, (SQLPOINTER)((SQLLEN)m_columnSize));
+				SetDescriptionField(hDesc.GetHandle(), columnNr, SQL_DESC_SCALE, (SQLPOINTER)m_decimalDigits);
+				SetDescriptionField(hDesc.GetHandle(), columnNr, SQL_DESC_DATA_PTR, (SQLPOINTER)m_pBuffer);
+				SetDescriptionField(hDesc.GetHandle(), columnNr, SQL_DESC_INDICATOR_PTR, (SQLPOINTER)m_pCb.get());
+				SetDescriptionField(hDesc.GetHandle(), columnNr, SQL_DESC_OCTET_LENGTH_PTR, (SQLPOINTER)m_pCb.get());
+			}
+			else
+			{
+				SQLRETURN ret = SQLBindCol(pHStmt->GetHandle(), columnNr, m_sqlCType, (SQLPOINTER*)m_pBuffer, GetBufferLength(), m_pCb.get());
+				THROW_IFN_SUCCESS(SQLBindCol, ret, SQL_HANDLE_STMT, pHStmt->GetHandle());
+			}
+			m_boundSelects.insert(boundHandleInfo);
+		}
+
+
+		/*!
+		* \brief	Unbinds from all handles bound.
+		* \throw	Exception
+		*/
+		void Unbind()
+		{
+			// unbind selects
+			std::set<ColumnBoundHandle>::iterator it = m_boundSelects.begin();
+			while (it != m_boundSelects.end())
+			{
+				ColumnBoundHandle bindInfo = *it;
+				exASSERT(bindInfo.m_pHStmt && bindInfo.m_pHStmt->IsAllocated());
+				it = UnbindSelect(bindInfo.m_columnNr, bindInfo.m_pHStmt);
+			}
+		}
+
+	protected:
+		std::set<ColumnBoundHandle>::iterator UnbindSelect(SQLUSMALLINT columnNr, ConstSqlStmtHandlePtr pHStmt)
+		{
+			exASSERT(columnNr >= 1);
+			exASSERT(pHStmt != NULL);
+			exASSERT(pHStmt->IsAllocated());
+			ColumnBoundHandle boundHandleInfo(pHStmt, columnNr);
+			std::set<ColumnBoundHandle>::iterator it = m_boundSelects.find(boundHandleInfo);
+			exASSERT_MSG(it != m_boundSelects.end(), L"Not bound to passed hStmt and column for Select on this buffer");
+
+			if (m_columnSize > 0 || m_decimalDigits > 0)
+			{
+				SqlDescHandle hDesc(pHStmt, RowDescriptorType::ROW);
+				SetDescriptionField(hDesc.GetHandle(), columnNr, SQL_DESC_TYPE, (SQLPOINTER)SQL_C_NUMERIC);
+				SetDescriptionField(hDesc.GetHandle(), columnNr, SQL_DESC_DATA_PTR, (SQLINTEGER)NULL);
+				SetDescriptionField(hDesc.GetHandle(), columnNr, SQL_DESC_INDICATOR_PTR, (SQLINTEGER)NULL);
+				SetDescriptionField(hDesc.GetHandle(), columnNr, SQL_DESC_OCTET_LENGTH_PTR, (SQLINTEGER)NULL);
+			}
+			else
+			{
+				SQLRETURN ret = SQLBindCol(pHStmt->GetHandle(), columnNr, m_sqlCType, NULL, 0, NULL);
+				THROW_IFN_SUCCEEDED(SQLBindCol, ret, SQL_HANDLE_STMT, pHStmt->GetHandle());
+			}
+			return m_boundSelects.erase(it);
+		}
 
 	private:
 		SQLPOINTER m_pBuffer;
 		SQLSMALLINT m_sqlCType;
 		SQLLEN m_bufferLength;
 
+		std::set<ColumnBoundHandle> m_boundSelects;
 	};
 	
 	// Array types
