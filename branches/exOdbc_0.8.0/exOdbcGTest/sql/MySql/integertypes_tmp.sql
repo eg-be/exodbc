def0e9bd5e13732c68666837439eddde6f2e53ad
eg <eg@7898c5a7-19f2-4356-953d-35c5c242a1bf>
2016-01-03 16:52:03 +0000

    try aPrepare Release-Branch 0.8.0

--- branches/exOdbc_0.8.0/exOdbcGTest/sql/MySql/integertypes_tmp.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS `integertypes_tmp`;
-CREATE TABLE `integertypes_tmp` (
-  `idintegertypes` int(11) NOT NULL,
-  `tsmallint` smallint(6) DEFAULT NULL,
-  `tint` int(11) DEFAULT NULL,
-  `tbigint` bigint(20) DEFAULT NULL,
-  PRIMARY KEY (`idintegertypes`),
-  UNIQUE KEY `idQueryTypes_UNIQUE` (`idintegertypes`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
