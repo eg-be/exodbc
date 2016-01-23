4a6ee2fe09b3053bbdd8077f1eeee29905297ad0
eg <23149233+eg-be@users.noreply.github.com>
2016-01-23 17:26:04 +0000

    Fix #204

--- exOdbc/include/LogHandler.h
+++ exOdbc/include/LogHandler.h
@@ -22,20 +22,33 @@
 
 namespace exodbc
 {
+	/*!
+	* \class	LogHandler
+	* \brief	Interface for a LogHandler. Must be able to do something with log messages.
+	*/
 	class EXODBCAPI LogHandler
 	{
 	public:
-		virtual void LogMessage(LogLevel level, const std::wstring& msg, const std::wstring& filename = L"", int line = 0, const std::wstring& functionname = L"") const = 0;
+		/*!
+		* \brief	Called from LogManager on every log message received. Do something usefull with it.
+		*/
+		virtual void OnLogMessage(LogLevel level, const std::wstring& msg, const std::wstring& filename = L"", int line = 0, const std::wstring& functionname = L"") const = 0;
 	};
 
 	typedef std::shared_ptr<LogHandler> LogHandlerPtr;
 
+	/*!
+	* \class	StdErrLogHandler
+	* \brief	A simple LogHandler that prints all messages nicely formated to stderr.
+	*/
 	class EXODBCAPI StdErrLogHandler
 		: public LogHandler
 	{
 	public:
-		virtual void LogMessage(LogLevel level, const std::wstring& msg, const std::wstring& filename = L"", int line = 0, const std::wstring& functionname = L"") const;
+		virtual void OnLogMessage(LogLevel level, const std::wstring& msg, const std::wstring& filename = L"", int line = 0, const std::wstring& functionname = L"") const;
 	};
+
+	typedef std::shared_ptr<StdErrLogHandler> StdErrLogHandlerPtr;
 
 
 }
