cdfd39b5c43feda58ee5833b2016ea32f20b75de
eg <eg@7898c5a7-19f2-4356-953d-35c5c242a1bf>
2016-02-04 18:10:54 +0000

    Tag Release-Branch 0.8.3

---
+++ tags/exOdbc_0.8.3/exOdbcGTest/sql/Access/blobtypes_tmp.sql
@@ -0,0 +1,10 @@
+DROP TABLE blobtypes_tmp;
+
+CREATE TABLE blobtypes_tmp (
+		idblobtypes Long PRIMARY KEY,
+		tblob BINARY(16),
+		tvarblob_20 VARBINARY(20)
+	);
+
+
+
---
+++ tags/exOdbc_0.8.3/exOdbcGTest/src/EnvironmentTest.h
@@ -0,0 +1,36 @@
+/*!
+* \file EnvironmentTest.h
+* \author Elias Gerber <eg@elisium.ch>
+* \date 27.07.2014
+* \copyright GNU Lesser General Public License Version 3
+* 
+* [Brief Header-file description]
+*/ 
+
+#pragma once
+
+// Same component headers
+#include "exOdbcGTest.h"
+#include "TestParams.h"
+
+// Other headers
+#include "gtest/gtest.h"
+
+// Forward declarations
+// --------------------
+
+namespace exodbctest
+{
+	// Structs
+	// -------
+
+	// Classes
+	// -------
+	class EnvironmentTest : public ::testing::Test
+	{
+	protected:
+		virtual void SetUp();
+		virtual void TearDown();
+	};
+} // namespace exodbctest
+
