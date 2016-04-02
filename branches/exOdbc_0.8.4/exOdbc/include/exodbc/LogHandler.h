9e9e0b574fdfd147d2efed122d7d227394ac7c0a
eg <eg@7898c5a7-19f2-4356-953d-35c5c242a1bf>
2016-04-02 17:42:31 +0000

    Prepare Release-Branch 0.8.4

---
+++ branches/exOdbc_0.8.4/exOdbc/include/exodbc/LogHandler.h
@@ -0,0 +1,65 @@
+/*!
+* \file LogHandler.h
+* \author Elias Gerber <eg@elisium.ch>
+* \date 23.01.2016
+* \brief Header file for Log Handler.
+* \copyright GNU Lesser General Public License Version 3
+*
+*/
+
+#pragma once
+
+// Same component headers
+#include "exOdbc.h"
+#include "LogManager.h"
+
+// Other headers
+// System headers
+#include <memory>
+
+// Forward declarations
+// --------------------
+
+namespace exodbc
+{
+	/*!
+	* \class	LogHandler
+	* \brief	Interface for a LogHandler. Must be able to do something with log messages.
+	*/
+	class EXODBCAPI LogHandler
+	{
+	public:
+		/*!
+		* \brief	Called from LogManager on every log message received. Do something usefull with it.
+		*/
+		virtual void OnLogMessage(LogLevel level, const std::wstring& msg, const std::wstring& filename = L"", int line = 0, const std::wstring& functionname = L"") const = 0;
+	};
+
+	typedef std::shared_ptr<LogHandler> LogHandlerPtr;
+
+	/*!
+	* \class	StdErrLogHandler
+	* \brief	A simple LogHandler that prints all messages nicely formated to stderr.
+	*/
+	class EXODBCAPI StdErrLogHandler
+		: public LogHandler
+	{
+	public:
+		virtual void OnLogMessage(LogLevel level, const std::wstring& msg, const std::wstring& filename = L"", int line = 0, const std::wstring& functionname = L"") const;
+	};
+	
+	typedef std::shared_ptr<StdErrLogHandler> StdErrLogHandlerPtr;
+
+	/*!
+	* \class	NullLogHandler
+	* \brief	A LogHandler that does nothing.
+	*/
+	class EXODBCAPI NullLogHandler
+		: public LogHandler
+	{
+	public:
+		virtual void OnLogMessage(LogLevel level, const std::wstring& msg, const std::wstring& filename = L"", int line = 0, const std::wstring& functionname = L"") const {};
+	};
+	
+	typedef std::shared_ptr<NullLogHandler> NullLogHandlerPtr;
+}
