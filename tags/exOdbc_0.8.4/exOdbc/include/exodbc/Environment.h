3bf797dfb5a252ac7ab11d0cc6263fe8750320dd
eg <eg@7898c5a7-19f2-4356-953d-35c5c242a1bf>
2016-04-04 21:36:16 +0000

    Tag Release-Branch 0.8.4

---
+++ tags/exOdbc_0.8.4/exOdbc/include/exodbc/Environment.h
@@ -0,0 +1,199 @@
+/*!
+ * \file Environment.h
+ * \author Elias Gerber <eg@elisium.ch>
+ * \date 25.07.2014
+ * \brief Header file for the Environment class and its helpers.
+ * \copyright GNU Lesser General Public License Version 3
+*/ 
+
+#pragma once
+
+// Same component headers
+#include "exOdbc.h"
+#include "InfoObject.h"
+#include "SqlHandle.h"
+
+// Other headers
+// System headers
+#include <string>
+#include <vector>
+
+
+// Forward declarations
+// --------------------
+
+namespace exodbc
+{
+	// Structs
+	// -------
+
+
+	// Classes
+	// -------
+	/*!
+	* \class Environment
+	*
+	* \brief Represents the ODBC-Environment. Every Database needs an environment, 
+	* but one environment can be used to open multiple databases.
+	* 
+	* This class will manage the ODBC Environment Handle that is required
+	* for all later operations in a SqlEnvHandlePtr. The shared_ptr will be valid
+	* during object lifetime as the SqlEnvHandle is allocated during construction.
+	* The Connection handle itself will be created during Init().
+	* 
+	*/
+	class EXODBCAPI Environment
+	{
+	public:
+		/*!
+		* \brief	Enable connection pooling on the driver manager.
+		* \details	This option must be set before the Environment is allocated
+		*			to enable it on further Environments.
+		* \see		SqlConnect https://msdn.microsoft.com/en-us/library/ms711810%28v=vs.85%29.aspx
+		* \throw	SqlResultException if SetSetEnvAttr does not succeed.
+		*/
+		static void EnableConnectionPooling(ConnectionPooling enablePooling);
+
+
+		/*!
+		 * \brief	Default constructor.
+		 * 			You must manually call Init() after creating the object.
+		 *			Sets ODBC Version to OdbcVersion::UNKNOWN.
+		 * \see		Init()
+		 * \throw	std::bad_alloc If creation of SqlEnvHandlePtr fails.
+		 */
+		Environment() noexcept;
+
+
+		/*!
+		 * \brief	Constructor. Calls Init() with passed odbcVersion.
+		 *			After Construction the ODBC Environment Handle is allocated
+		 *			and OdbcVersion is set to passed odbcVersion.
+		 *
+		 * \param	odbcVersion	The ODBC version to set.
+		 * \throw	std::bad_alloc If creation of SqlEnvHandlePtr fails.
+		 * \throw	Exception If Init() fails.
+		 */
+		Environment(OdbcVersion odbcVersion);
+
+
+		/*!
+		* \brief	Copy Constructor.
+		* \details	If the passed Environment has a valid ODBC Version set, the environment
+		*			will be created with the same version by calling Init().
+		* \param	other Environment to copy.
+		* \throw	Exception If Init() fails.
+		*/
+		Environment(const Environment& other);
+
+
+		/*!
+		 * \brief		Destructor. 
+		 */
+		~Environment();
+
+
+		/*!
+		* \brief	Create new Environment using passed ODBC version. Created Environment
+		*			is wrapped into a shared_ptr.
+		*/
+		static std::shared_ptr<Environment> Create(OdbcVersion odbcVersion);
+
+
+		/*!
+		* \brief	Must be called if Environment has been created using Default Constructor.
+		* \details	Can only be called once. Allocates the ODBC Environment handle and sets the
+		*			passed OdbcVersion.
+		* \param	odbcVersion The ODBC Version to set on this Environment.
+		* \see		IsEnvHandleAllocated()
+		* \throw	Exception
+		*/
+		void Init(OdbcVersion odbcVersion);
+
+
+		/*!
+		 * \brief	Returns true if the SqlEnvHandle has an allocated ODBC Environment handle.
+		 */
+		bool			IsEnvHandleAllocated() const { exASSERT(m_pHEnv); return m_pHEnv->IsAllocated(); };
+
+
+		/*!
+		* \brief	Returns the shared_ptr to the SqlEnvHandle holding the ODBC Environment handle.
+		*			Note that this does not guarantee that the SqlEnvHandle returned actually has 
+		*			Environment Handle allocated or that the pointer is not NULL.
+		*/
+		ConstSqlEnvHandlePtr	GetSqlEnvHandle() const noexcept { return m_pHEnv; };
+
+		
+		/*!
+		 * \brief	Sets ODBC version. Updates internally cached value by calling
+		 *			ReadOdbcVersion() if setting seemed to be successfull.
+		 *
+		 * \param	version	The version.
+		 * \throw	Exception if no ODBC Environment Handle is allocated, or setting the version fails.
+		 */
+		void			SetOdbcVersion(OdbcVersion version);		
+
+
+		/*!
+		 * \brief	Reads the ODBC version from the environment and updates internally
+		 *			cached value.
+		 * 
+		 * \return	The ODBC version or OV_UNKNOWN if read version is unknown.
+		 * \throw	Exception If no ODBC Environment Handle is allocated, or reading the version fails.
+		 */
+		OdbcVersion		ReadOdbcVersion() const;
+
+
+		/*!
+		* \brief	Gets the cached ODBC version if the cached version is not OV_UNKNWON.
+		*			If cached version is OV_UNKOWN and the environment handle is allocated,
+		*			it will try to read the odbc version using ReadOdbcVersion().
+		*
+		* \return	The ODBC version or OV_UNKNOWN if the handle is not allocated yet.
+		* \throw	Exception If reading the version fails.
+		*/
+		OdbcVersion		GetOdbcVersion() const;
+
+		/*!
+		* \enum		ListMode
+		* \brief	What data sources to list in ListDataSources()
+		*/
+		enum class ListMode 
+		{ 
+			All,	///< System and User entries.
+			System, //< Only system entries.
+			User	//< Only user entries.
+		};
+
+		/*!
+		 * \brief		List data sources.
+		 * \details	List the Data Source Names (DSN) entries available.
+		 *				Fails if no ODBC Environment Handle is allocated.
+		 * \param	mode	Decide to list all DSNs, or only user / system DSNs.
+		 * \return	Found Data Source Names.
+		 * \throw	Exception
+		 */		
+		DataSourcesVector ListDataSources(ListMode mode) const;
+
+
+		/*!
+		* \brief	Set if connection opened from this environment must
+		*			match strict or not if connection pooling is enabled.
+		* \see		EnableConnectionPooling.
+		* \throw	Exception If no handle is allocated, or setting the attribute fails.
+		*/
+		void SetConnctionPoolingMatch(ConnectionPoolingMatch matchMode);
+
+	private:		
+		// Members
+		// -------
+		SqlEnvHandlePtr m_pHEnv;	///< Environment handle
+		mutable OdbcVersion m_odbcVersion; ///< Cached ODBC version
+
+	};  // class Environment
+
+	typedef std::shared_ptr<Environment> EnvironmentPtr;
+	typedef std::shared_ptr<const Environment> ConstEnvironmentPtr;
+}
+
---
+++ tags/exOdbc_0.8.4/exOdbc/src/SpecializedExceptions.cpp
@@ -0,0 +1,144 @@
+/*!
+* \file SpecializedExceptions.cpp
+* \author Elias Gerber <eg@elisium.ch>
+* \date 23.01.2016
+* \brief Source file for specialized Exceptions and its helpers.
+* \copyright GNU Lesser General Public License Version 3
+*
+*/
+
+#include "stdafx.h"
+
+// Own header
+#include "SpecializedExceptions.h"
+
+// Same component headers
+// Other headers
+// Debug
+#include "DebugNew.h"
+
+// Static consts
+// -------------
+
+using namespace std;
+
+namespace exodbc
+{
+	// Construction
+	// ------------
+
+	// Destructor
+	// -----------
+
+	// Implementation
+	// --------------
+	SqlResultException::SqlResultException(const std::wstring& sqlFunctionName, SQLRETURN ret, const std::wstring& msg /* = L"" */) noexcept
+		: Exception(msg)
+		, m_ret(0)
+	{
+		// We have no Error-Info to fetch
+		BuildErrorMsg(sqlFunctionName, ret);
+		m_what = utf16ToUtf8(ToString());
+	}
+
+
+	SqlResultException::SqlResultException(const std::wstring& sqlFunctionName, SQLRETURN ret, SQLSMALLINT handleType, SQLHANDLE handle, const std::wstring& msg /* = L"" */) noexcept
+		: Exception(msg)
+		, m_ret(ret)
+	{
+		// Fetch error-information from the database and build the error-msg
+		FetchErrorInfo(handleType, handle);
+		BuildErrorMsg(sqlFunctionName, ret);
+		m_what = utf16ToUtf8(ToString());
+	}
+
+
+	void SqlResultException::FetchErrorInfo(SQLSMALLINT handleType, SQLHANDLE handle) noexcept
+	{
+		try
+		{
+			m_errors = GetAllErrors(handleType, handle);
+		}
+		catch (const Exception& e)
+		{
+			// Append a faked Error-Info
+			SErrorInfo errInfo;
+			errInfo.ErrorHandleType = handleType;
+			errInfo.SqlState[0] = 0;
+			errInfo.NativeError = 0;
+			errInfo.Msg = (boost::wformat(L"Failed to fetch Error-Info: %s") % e.ToString()).str();
+			m_errors.push_back(errInfo);
+		}
+	}
+
+
+	void SqlResultException::BuildErrorMsg(const std::wstring& sqlFunctionName, SQLRETURN ret) noexcept
+	{
+		try
+		{
+			std::wstringstream ws;
+			ws << L"SQL-Function " << sqlFunctionName << L" returned ";
+			ws << SqlReturn2s(ret) << L"(" << ret << L") with " << m_errors.size() << " ODBC-Error(s):";
+			m_errorMsg = ws.str();
+		}
+		catch (const Exception& e)
+		{
+			HIDE_UNUSED(e);
+			m_errorMsg = L"Failed to BuildErrorMsg";
+		}
+	}
+
+
+	std::wstring SqlResultException::ToString() const noexcept
+	{
+		std::wstringstream ws;
+		ws << Exception::ToString();
+		if (!m_errorMsg.empty())
+		{
+			ws << L": " << m_errorMsg;
+		}
+		ws << L"\n";
+		for (size_t i = 0; i < m_errors.size(); i++)
+		{
+			ws << m_errors[i].ToString() << L"\n";
+		}
+		return ws.str();
+	}
+
+
+	std::wstring NotSupportedException::ToString() const noexcept
+	{
+		std::wstringstream ws;
+		ws << Exception::ToString();
+		ws << L"Not Supported ";
+		switch (m_notSupported)
+		{
+		case Type::SQL_C_TYPE:
+			ws << L"SQL C Type " << SqLCType2s(m_smallInt) << L" (" << m_smallInt << L")";
+			break;
+		case Type::SQL_TYPE:
+			ws << L"SQL Type" << SqlType2s(m_smallInt) << L" (" << m_smallInt << L")";
+			break;
+		}
+		return ws.str();
+	}
+
+
+	std::wstring WrapperException::ToString() const noexcept
+	{
+		std::wstringstream ws;
+		ws << Exception::ToString();
+		ws << utf8ToUtf16(m_innerExceptionMsg);
+		return ws.str();
+	}
+
+
+	std::wstring NullValueException::ToString() const noexcept
+	{
+		std::wstringstream ws;
+		ws << Exception::ToString();
+		ws << L"Cannot fetch value of column '" << m_columnName << L"', the value is NULL.";
+		return ws.str();
+	}
+
+}
