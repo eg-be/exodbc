9e82063606a3d8be29159725e7ae35478a4a9cc0
eg <eg@7898c5a7-19f2-4356-953d-35c5c242a1bf>
2016-04-10 15:54:06 +0000

    #213 again

--- trunk/exOdbc/src/Table.cpp
+++ trunk/exOdbc/src/Table.cpp
@@ -178,7 +178,7 @@ namespace exodbc
 				m_execStmtSelect.Init(m_pDb, forwardOnlyCursors);
 
 				// Create the buffer required for counts
-				m_pSelectCountResultBuffer = UBigIntColumnBuffer::Create(L"", ColumnFlag::CF_SELECT);
+				m_pSelectCountResultBuffer = UBigIntColumnBuffer::Create(L"", SQL_UNKNOWN_TYPE, ColumnFlag::CF_SELECT);
 			}
 			if (TestAccessFlag(TableAccessFlag::AF_DELETE_PK))
 			{
