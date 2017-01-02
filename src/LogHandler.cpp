6277e50911b41dfb4980aaff54723f84c4fc73a8
eg <23149233+eg-be@users.noreply.github.com>
2017-01-02 01:02:19 +0000

    Create a branch and try to make it utf-8 only. see #214

---
+++ src/LogHandler.cpp
@@ -0,0 +1,158 @@
+/*!
+* \file LogHandler.cpp
+* \author Elias Gerber <eg@elisium.ch>
+* \date 23.01.2016
+* \brief Source file for name objects.
+* \copyright GNU Lesser General Public License Version 3
+*
+*/
+
+// Own header
+#include "LogHandler.h"
+
+// Same component headers
+// Other headers
+#include <iostream>
+#include <sstream>
+#include <iomanip>
+#include <ctime>
+
+// Debug
+#include "DebugNew.h"
+
+// Static consts
+// -------------
+
+using namespace std;
+
+namespace exodbc
+{
+	std::wstring FormatLogMessage(LogLevel level, const std::wstring& msg, const std::wstring& filename /* = L"" */, int line /* = 0 */, const std::wstring& functionname /* = L"" */)
+	{
+		std::wstringstream ws;
+
+		switch (level)
+		{
+		case exodbc::LogLevel::Error: \
+			ws << L"ERROR"; break; \
+		case exodbc::LogLevel::Warning: \
+			ws << L"WARNING"; break; \
+		case exodbc::LogLevel::Info: \
+			ws << L"INFO"; break; \
+		case exodbc::LogLevel::Debug: \
+			ws << L"DEBUG"; break; \
+		}
+
+		bool haveSourceInfo = !filename.empty() || line > 0 || !functionname.empty();
+		if (haveSourceInfo)
+			ws << L" [";
+
+		if (!filename.empty())
+			ws << filename;
+		if (line > 0)
+			ws << L"(" << line << L")";
+		else if (!filename.empty())
+			ws << L"(\?\?)";
+		if (!functionname.empty())
+			ws << functionname;
+
+		if (haveSourceInfo)
+			ws << L"]";
+
+		ws << L": " << msg;
+
+		return ws.str();
+	}
+
+	void StdErrLogHandler::OnLogMessage(LogLevel level, const std::wstring& msg, const std::wstring& filename /* = L"" */, int line /* = 0 */, const std::wstring& functionname /* = L"" */) const
+	{
+		wcout << FormatLogMessage(level, msg, filename, line, functionname) << endl;
+	}
+
+
+	bool StdErrLogHandler::operator==(const LogHandler& other) const
+	{
+		try
+		{
+			// all StdLogHandlers are equal
+			dynamic_cast<const StdErrLogHandler&>(other);
+			return true;
+		}
+		catch (const std::bad_cast& ex) 
+		{
+			HIDE_UNUSED(ex);
+			return false;
+		}
+	}
+
+
+	bool NullLogHandler::operator==(const LogHandler& other) const
+	{
+		try
+		{
+			// all NullLogHandler are equal
+			dynamic_cast<const NullLogHandler&>(other);
+			return true;
+		}
+		catch (const std::bad_cast& ex)
+		{
+			HIDE_UNUSED(ex);
+			return false;
+		}
+	}
+
+
+	FileLogHandler::FileLogHandler(const std::wstring& filepath, bool prependTimestamp)
+		: m_prependTimestamp(prependTimestamp)
+		, m_filepath(filepath)
+		, m_firstMessage(true)
+	{
+	}
+
+
+	FileLogHandler::~FileLogHandler()
+	{
+		if (m_filestream.is_open())
+		{
+			m_filestream.close();
+		}
+	}
+
+
+	void FileLogHandler::OnLogMessage(LogLevel level, const std::wstring& msg, const std::wstring& filename /* = L"" */, int line /* = 0 */, const std::wstring& functionname /* = L"" */) const
+	{
+		if (m_firstMessage)
+		{
+			m_filestream = std::wofstream(SQLAPICHARCONVERT(m_filepath), std::ofstream::out);
+			m_firstMessage = false;
+		}
+		if (m_filestream.is_open())
+		{
+			if (m_prependTimestamp)
+			{
+                std::time_t t = std::time(nullptr);
+                std::tm tm = *std::localtime(&t);
+				m_filestream << std::put_time(&tm, L"%d-%m-%Y %H-%M-%S") << L": ";
+			}
+
+			m_filestream << FormatLogMessage(level, msg, filename, line, functionname) << endl;
+		}
+	}
+
+
+	bool FileLogHandler::operator==(const LogHandler& other) const
+	{
+		try
+		{
+			// equal if the same file
+			auto& otherFileLogger = dynamic_cast<const FileLogHandler&>(other);
+			return otherFileLogger.GetFilepath() == GetFilepath();
+		}
+		catch (const std::bad_cast& ex)
+		{
+			HIDE_UNUSED(ex);
+			return false;
+		}
+	}
+
+}
---
+++ test/EnumFlagsTest.cpp
@@ -0,0 +1,74 @@
+/*!
+* \file EnumFlagsTest.cpp
+* \author Elias Gerber <eg@elisium.ch>
+* \date 21.11.2015
+* \copyright GNU Lesser General Public License Version 3
+*
+* [Brief CPP-file description]
+*/
+
+// Own header
+#include "EnumFlagsTest.h"
+
+// Same component headers
+// Other headers
+#include "exodbc/EnumFlags.h"
+
+// Debug
+#include "DebugNew.h"
+
+// Static consts
+// -------------
+
+// Construction
+// -------------
+
+// Destructor
+// -----------
+
+// Implementation
+// --------------
+using namespace std;
+using namespace exodbc;
+
+namespace exodbctest
+{
+
+	TEST_F(EnumFlagsTest, InitFlags)
+	{
+		ColumnFlags flags;
+		
+		// No flag must be set
+		EXPECT_FALSE(flags.Test(ColumnFlag::CF_SELECT));
+		EXPECT_FALSE(flags.Test(ColumnFlag::CF_UPDATE));
+		EXPECT_FALSE(flags.Test(ColumnFlag::CF_INSERT));
+		EXPECT_FALSE(flags.Test(ColumnFlag::CF_NULLABLE));
+		EXPECT_FALSE(flags.Test(ColumnFlag::CF_PRIMARY_KEY));
+	}
+
+
+	TEST_F(EnumFlagsTest, ConstructFlags)
+	{
+		ColumnFlags flags(ColumnFlag::CF_NULLABLE | ColumnFlag::CF_INSERT);
+
+		// No flag must be set except constructed
+		EXPECT_FALSE(flags.Test(ColumnFlag::CF_SELECT));
+		EXPECT_FALSE(flags.Test(ColumnFlag::CF_UPDATE));
+		EXPECT_TRUE(flags.Test(ColumnFlag::CF_INSERT));
+		EXPECT_TRUE(flags.Test(ColumnFlag::CF_NULLABLE));
+		EXPECT_FALSE(flags.Test(ColumnFlag::CF_PRIMARY_KEY));
+
+	}
+
+	TEST_F(EnumFlagsTest, SetAndClearFlag)
+	{
+		ColumnFlags f;
+
+		EXPECT_FALSE(f.Test(ColumnFlag::CF_NULLABLE));
+		f.Set(ColumnFlag::CF_NULLABLE);
+		EXPECT_TRUE(f.Test(ColumnFlag::CF_NULLABLE));
+		f.Clear(ColumnFlag::CF_NULLABLE);
+		EXPECT_FALSE(f.Test(ColumnFlag::CF_NULLABLE));
+	}
+
+} //namespace exodbc
