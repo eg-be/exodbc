be5c4268b2a1d008658da1d26a4b2d563111584f
eg <23149233+eg-be@users.noreply.github.com>
2017-01-02 01:08:46 +0000

    Exception is utf-8 aware

--- src/Exception.cpp
+++ src/Exception.cpp
@@ -40,37 +40,37 @@ namespace exodbc
 	}
 
 
-	void Exception::SetSourceInformation(int line, const std::wstring& fileName, const std::wstring& functionName) noexcept
+	void Exception::SetSourceInformation(int line, const std::string& fileName, const std::string& functionName) noexcept
 	{
 		m_line = line;
 		m_file = fileName;
 		m_functionname = functionName;
 
-		m_what = utf16ToUtf8(ToString());
+		m_what = ToString();
 	}
 
 
-	std::wstring Exception::ToString() const noexcept
+	std::string Exception::ToString() const noexcept
 	{
-		std::wstringstream ws;
-		ws << GetName();
-		ws << L"[";
+		std::stringstream ss;
+		ss << GetName();
+		ss << u8"[";
 		if (m_line > 0)
 		{
 			// keep only filename
-			std::wstring fname = m_file;
-			size_t pos = m_file.rfind(L"\\");
+			std::string fname = m_file;
+			size_t pos = m_file.rfind(u8"\\");
 			if (pos != std::wstring::npos && m_file.length() > pos + 1)
 			{
 				fname = m_file.substr(pos + 1);
 			}
-			ws << fname << L"(" << m_line << L")@" << m_functionname;
+			ss << fname << u8"(" << m_line << u8")@" << m_functionname;
 		}
-		ws << L"]";
+		ss << u8"]";
 		if (!m_msg.empty())
 		{
-			ws << L": " << m_msg;
+			ss << u8": " << m_msg;
 		}
-		return ws.str();
+		return ss.str();
 	}
 }
