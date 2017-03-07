d49e4238f8f91e7b872cc7a3b89397121f80a598
eg <eg@7898c5a7-19f2-4356-953d-35c5c242a1bf>
2017-03-07 21:33:23 +0000

    delete stuff that wasnt deleted by the merges

--- trunk/exOdbcGTest/sql/Access/chartypes_tmp.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE chartypes_tmp;
-
-CREATE TABLE chartypes_tmp (
-		idchartypes Long PRIMARY KEY,
-		tvarchar VARCHAR(128),
-		tchar VARCHAR(128),
-		tvarchar_10 VARCHAR(10),
-		tchar_10 VARCHAR(10)
-	);
--- trunk/exOdbcGTest/sql/Access/integertypes_tmp.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE integertypes_tmp;
-
-CREATE TABLE integertypes_tmp (
-		idintegertypes Long PRIMARY KEY,
-		tsmallint Integer,
-		tint Long,
-		tbigint Long
-	);
