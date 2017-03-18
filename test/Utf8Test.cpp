e2fc3d3b40a07ff3260bf4882f80a09f1a3cad0b
eg <23149233+eg-be@users.noreply.github.com>
2017-03-18 15:01:07 +0000

    more generic, we cannot know as what type unicode data is stored in the db

--- test/Utf8Test.cpp
+++ test/Utf8Test.cpp
@@ -61,31 +61,32 @@ namespace exodbctest
 	}
 	
 
-	TEST_F(Utf8Test, OpenAsSqlChar)
+	TEST_F(Utf8Test, OpenAuto)
 	{
 		// Test that for every database auto columns with utf8 data can be created
 		Table t(m_pDb, TableAccessFlag::AF_READ, GetTableName(TableId::UTF8_TABLE));
 
-		// For utf-8 data enforce binding to char instead of wchar
-		auto bindMap = std::make_shared<DefaultSql2BufferMap>(OdbcVersion::V_3);
-		bindMap->RegisterType(SQL_WVARCHAR, SQL_C_CHAR);
-		// In sql server we used a varbinary column in the table.
-		// lets override binding VARBINARY to SQL_C_BINARAY (which is SQL_C_CHAR probably anyway)
-		if (m_pDb->GetDbms() == DatabaseProduct::MS_SQL_SERVER)
-		{
-			bindMap->RegisterType(SQL_VARBINARY, SQL_C_CHAR);
-		}
-		t.SetSql2BufferTypeMap(bindMap);
+		//// For utf-8 data enforce binding to char instead of wchar
+		//auto bindMap = std::make_shared<DefaultSql2BufferMap>(OdbcVersion::V_3);
+		//bindMap->RegisterType(SQL_WVARCHAR, SQL_C_CHAR);
+		//// In sql server we used a varbinary column in the table.
+		//// lets override binding VARBINARY to SQL_C_BINARAY (which is SQL_C_CHAR probably anyway)
+		//if (m_pDb->GetDbms() == DatabaseProduct::MS_SQL_SERVER)
+		//{
+		//	bindMap->RegisterType(SQL_VARBINARY, SQL_C_CHAR);
+		//}
+		//t.SetSql2BufferTypeMap(bindMap);
 
 		t.Open();
 		EXPECT_EQ(2, t.GetColumnBufferCount());
 		auto utf8Col = t.GetColumnBufferPtrVariant(1);
 		
-		// Test if we now have a binary SQL_C_CHAR column that might hold our utf-8 data:
-		SqlCTypeVisitor cTypeV;
-		//SQLSMALLINT t1 = boost::apply_visitor(cTypeV, utf8Col);
-		//string s = SqlCType2OdbcS(t1);
-		EXPECT_EQ(SQL_C_CHAR, boost::apply_visitor(cTypeV, utf8Col));
+		// Depending on the database used, the concrete column type created could be any of
+		// WCharColumnBuffer or CharColumnBuffer
+		// But all should have a column with a query name ending with 'content' (the
+		// name of the column)
+		string queryName = boost::apply_visitor(QueryNameVisitor(), utf8Col);
+		EXPECT_TRUE(boost::algorithm::iends_with(queryName, u8"content"));
 	}
 
 
@@ -105,11 +106,11 @@ namespace exodbctest
 		//}
 		//u8Table.SetSql2BufferTypeMap(bindMap);
 
-		u8Table.Open();
-		string idColName = GetIdColumnName(TableId::UTF8_TABLE);
-		string sqlWhere = boost::str(boost::format(u8"%s = 1") % idColName);
-		u8Table.Select(sqlWhere);
-		u8Table.SelectNext();
+		//u8Table.Open();
+		//string idColName = GetIdColumnName(TableId::UTF8_TABLE);
+		//string sqlWhere = boost::str(boost::format(u8"%s = 1") % idColName);
+		//u8Table.Select(sqlWhere);
+		//u8Table.SelectNext();
 		
 		//auto Content;
 		//if (m_pDb->GetDbms() == DatabaseProduct::MS_SQL_SERVER)
@@ -120,15 +121,15 @@ namespace exodbctest
 		//	int p = 3;
 		//}
 		//else
-		{
-			auto pContent = u8Table.GetColumnBufferPtr<WCharColumnBufferPtr>(1);
-			wstring s1 = pContent->GetWString();
-			string s2 = u8"После смерти отца оставил учёбу и поступил на службу газетным репортёром";
-			wstring w3 = utf8ToUtf16(s2);
-			EXPECT_EQ(w3, s1);
-			int p = 3;
-		}
-		int p = 3;
+		//{
+		//	auto pContent = u8Table.GetColumnBufferPtr<WCharColumnBufferPtr>(1);
+		//	wstring s1 = pContent->GetWString();
+		//	string s2 = u8"После смерти отца оставил учёбу и поступил на службу газетным репортёром";
+		//	wstring w3 = utf8ToUtf16(s2);
+		//	EXPECT_EQ(w3, s1);
+		//	int p = 3;
+		//}
+		//int p = 3;
 	}
 
 } // namespace exodbctest
