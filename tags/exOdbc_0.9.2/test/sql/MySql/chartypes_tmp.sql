8cdb0200b252de8db9d9e6cc28b65af0d3140be8
eg <eg@7898c5a7-19f2-4356-953d-35c5c242a1bf>
2017-03-25 17:34:33 +0000

    tagged too early

--- tags/exOdbc_0.9.2/test/sql/MySql/chartypes_tmp.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS `chartypes_tmp`;
-
-CREATE TABLE `chartypes_tmp` (
-  `idchartypes` int(11) NOT NULL,
-  `tvarchar` varchar(128) DEFAULT NULL,
-  `tchar` char(128) DEFAULT NULL,
-  PRIMARY KEY (`idchartypes`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
