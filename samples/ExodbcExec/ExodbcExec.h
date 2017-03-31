f00e32fd9be7f6e7bdc79c764ad48004d9e5899f
eg <23149233+eg-be@users.noreply.github.com>
2017-03-31 21:37:42 +0000

    start #264 Add sqlexec sample

---
+++ samples/ExodbcExec/ExodbcExec.h
@@ -0,0 +1,48 @@
+﻿/*!
+* \file OdbcExec.h
+* \author Elias Gerber <eg@elisium.ch>
+* \date 31.03.2017
+* \brief Header file for odbcexec
+* \copyright GNU Lesser General Public License Version 3
+*
+*/
+
+#pragma once
+
+// Same component headers
+// Other headers
+#include "exodbc/Database.h"
+
+// System headers
+#include <string>
+#include <vector>
+
+// Forward declarations
+// --------------------
+
+namespace exodbcexec
+{
+	// Typedefs
+	// --------
+
+	// Structs
+	// -------
+
+	// Classes
+	// -------
+
+	/*!
+	* \class ExodbcExec
+	* \brief An e
+	*/
+	class ExodbcExec
+	{
+	public:
+		ExodbcExec(exodbc::DatabasePtr pDb);
+
+		void Run(const CommandGenerator& comGen);
+
+	private:
+		exodbc::DatabasePtr m_pDb;
+	};
+}
