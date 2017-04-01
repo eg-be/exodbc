f800dfebc962072114375baff059041c03a6a821
eg <23149233+eg-be@users.noreply.github.com>
2017-04-01 23:15:02 +0000

    almost

--- samples/ExodbcExec/ExodbcExec.cpp
+++ samples/ExodbcExec/ExodbcExec.cpp
@@ -57,11 +57,15 @@ void printUsage()
 	WRITE_STDOUT_ENDL(u8" --silent                Hides all output.");
 	WRITE_STDOUT_ENDL(u8" --odbcVersion <version> Set ODBC Version to use. Valid values are '2', '3'");
 	WRITE_STDOUT_ENDL(u8"                         or '3.8'. Default is '3'.");
+	WRITE_STDOUT_ENDL(u8" --forwardOnlyCursors    Disables the commands '!prev', '!first' and");
+	WRITE_STDOUT_ENDL(u8"                         '!last'. Only '!next' can be used to iterate");
+	WRITE_STDOUT_ENDL(u8"                         records. If not set, positionable cursors are");
+	WRITE_STDOUT_ENDL(u8"                         enabled by default.");
 	WRITE_STDOUT_ENDL(u8" --exitOnError           Exits with a non-zero status if SQL execution");
 	WRITE_STDOUT_ENDL(u8"                         fails or any SQL related call fails. Default is to");
 	WRITE_STDOUT_ENDL(u8"                         log a warning and continue.");
-	WRITE_STDOUT_ENDL(u8" --logLevel <level> Set the Log Level. <level> can be 'Debug', Info',");
-	WRITE_STDOUT_ENDL(u8"                    'Warning' or 'Error'. Default is 'Info'.");
+	WRITE_STDOUT_ENDL(u8" --logLevel <level>      Set the Log Level. <level> can be 'Debug', Info',");
+	WRITE_STDOUT_ENDL(u8"                         'Warning' or 'Error'. Default is 'Info'.");
 	WRITE_STDOUT_ENDL(u8" --help                  Show this text and return with -1.");
 }
 
@@ -86,12 +90,14 @@ int main(int argc, char* argv[])
 		const std::string csKey = u8"-CS";
 		const std::string logLevelKey = u8"--logLevel";
 		const std::string silentKey = u8"--silent";
+		const std::string forwardOnlyCursorsKey = u8"--forwardOnlyCursors";
 		const std::string exitOnErrorKey = u8"--exitOnError";
 		const std::string odbcVersionKey = u8"--odbcVersion";
 		std::string userValue, passValue, dsnValue;
 		std::string csValue;
 		bool silent = false;
 		bool exitOnError = false;
+		bool forwardOnlyCursorsValue = false;
 		OdbcVersion odbcVersionValue = OdbcVersion::V_3;
 		LogLevel logLevelValue = LogLevel::Info;
 		for (int i = 0; i < argc; i++)
@@ -130,6 +136,10 @@ int main(int argc, char* argv[])
 			if (ba::equals(arg, silentKey))
 			{
 				silent = true;
+			}
+			if (ba::equals(arg, forwardOnlyCursorsKey))
+			{
+				forwardOnlyCursorsValue = true;
 			}
 			if (ba::equals(arg, exitOnErrorKey))
 			{
@@ -199,7 +209,7 @@ int main(int argc, char* argv[])
 		LOG_INFO(boost::str(boost::format(u8"Successfully connected to database system '%s' using driver '%s'") % dbInfo.GetDbmsName() % dbInfo.GetDriverName()));
 
 		// Start exodbcexec on that db:
-		exodbcexec::ExodbcExec exec(pDb, exitOnError);
+		exodbcexec::ExodbcExec exec(pDb, exitOnError, forwardOnlyCursorsValue);
 		exodbcexec::InputGeneratorPtr pGen = std::make_shared<exodbcexec::StdInGenerator>();
 		return exec.Run(pGen);
 	}
@@ -213,19 +223,21 @@ int main(int argc, char* argv[])
 namespace exodbcexec
 {
 
-	const std::set<std::string> ExodbcExec::COMMAND_EXIT = { u8"!exit", u8"!e" };
+	const std::set<std::string> ExodbcExec::COMMAND_EXIT = { u8"!exit", u8"!e", u8"!quit", u8"!q" };
 	const std::set<std::string> ExodbcExec::COMMAND_HELP = { u8"!help", u8"!h" };
-	const std::set<std::string> ExodbcExec::COMMAND_PRINT = { u8"!print", u8"!p" };
-	const std::set<std::string> ExodbcExec::COMMAND_SELECT_NEXT = { u8"!next", u8"!n" };
-	const std::set<std::string> ExodbcExec::COMMAND_SELECT_PREV = { u8"!prev", u8"!r" };
-	const std::set<std::string> ExodbcExec::COMMAND_SELECT_FIRST = { u8"!first", u8"!f" };
-	const std::set<std::string> ExodbcExec::COMMAND_SELECT_LAST = { u8"!last", u8"!l" };
+	const std::set<std::string> ExodbcExec::COMMAND_PRINT_ALL = { u8"!printAll", u8"!pa" };
+	const std::set<std::string> ExodbcExec::COMMAND_PRINT_CURRENT = { u8"!printCurrent", u8"!pc" };
+	const std::set<std::string> ExodbcExec::COMMAND_SELECT_NEXT = { u8"!next", u8"!sn" };
+	const std::set<std::string> ExodbcExec::COMMAND_SELECT_PREV = { u8"!prev", u8"!sp" };
+	const std::set<std::string> ExodbcExec::COMMAND_SELECT_FIRST = { u8"!first", u8"!sf" };
+	const std::set<std::string> ExodbcExec::COMMAND_SELECT_LAST = { u8"!last", u8"!sl" };
 
-	ExodbcExec::ExodbcExec(exodbc::DatabasePtr pDb, bool exitOnError)
+	ExodbcExec::ExodbcExec(exodbc::DatabasePtr pDb, bool exitOnError, bool forwardOnlyCursors)
 		: m_pDb(pDb)
 		, m_exitOnError(exitOnError)
+		, m_forwardOnlyCursors(forwardOnlyCursors)
 	{
-		m_stmt.Init(m_pDb, true);
+		m_stmt.Init(m_pDb, m_forwardOnlyCursors);
 	}
 
 
@@ -244,28 +256,37 @@ namespace exodbcexec
 				{
 					PrintHelp();
 				}
-				else if (COMMAND_PRINT.find(command) != COMMAND_PRINT.end())
+				else if (COMMAND_PRINT_ALL.find(command) != COMMAND_PRINT_ALL.end())
 				{
 					Print(PrintMode::All);
 				}
+				else if(COMMAND_PRINT_CURRENT.find(command) != COMMAND_PRINT_CURRENT.end())
+				{
+					Print(PrintMode::Current);
+				}
 				else if (COMMAND_SELECT_NEXT.find(command) != COMMAND_SELECT_NEXT.end())
 				{
-
+					Select(SelectMode::Next);
 				}
-				else if (COMMAND_SELECT_PREV.find(command) != COMMAND_SELECT_PREV.end())
+				else if (!m_forwardOnlyCursors && COMMAND_SELECT_PREV.find(command) != COMMAND_SELECT_PREV.end())
 				{
-
+					Select(SelectMode::Prev);
 				}
-				else if (COMMAND_SELECT_FIRST.find(command) != COMMAND_SELECT_FIRST.end())
+				else if (!m_forwardOnlyCursors && COMMAND_SELECT_FIRST.find(command) != COMMAND_SELECT_FIRST.end())
 				{
-
+					Select(SelectMode::First);
 				}
-				else if (COMMAND_SELECT_LAST.find(command) != COMMAND_SELECT_LAST.end())
+				else if (!m_forwardOnlyCursors && COMMAND_SELECT_LAST.find(command) != COMMAND_SELECT_LAST.end())
 				{
-
+					Select(SelectMode::Last);
 				}
 				else
 				{
+					if (ba::starts_with(command, u8"!"))
+						LOG_WARNING(boost::str(boost::format(u8"Input starts with '!' but is not recognized as a command, executing as SQL.")));
+
+					// Before executing, unbind any bound columns
+					UnbindColumns();
 					LOG_INFO(boost::str(boost::format(u8"Executing '%s'") % command));
 					auto start = std::chrono::high_resolution_clock::now();
 					m_stmt.ExecuteDirect(command);
@@ -274,6 +295,11 @@ namespace exodbcexec
 					auto millis = std::chrono::duration_cast<std::chrono::milliseconds>(elapsed);
 					LOG_INFO(boost::str(boost::format(u8"Success. Execution took %1%ms.")
 						% millis.count()));
+					// And bind columns, if there are any
+					if (m_stmt.GetNrOfColumns() > 0)
+					{
+						BindColumns();
+					}
 				}
 			}
 			catch (const exodbc::SqlResultException& sre)
@@ -295,8 +321,10 @@ namespace exodbcexec
 	}
 
 
-	void ExodbcExec::Print(ExodbcExec::PrintMode mode)
+	void ExodbcExec::BindColumns()
 	{
+		exASSERT(m_currentColumns.empty());
+
 		SQLSMALLINT nrOfCols = m_stmt.GetNrOfColumns();
 		if (nrOfCols == 0)
 		{
@@ -304,10 +332,7 @@ namespace exodbcexec
 			return;
 		}
 
-		m_stmt.UnbindColumns();
-
 		// Bind columns
-		std::vector<StringColumnWrapper> stringCols;
 		for (SQLSMALLINT colNr = 1; colNr <= nrOfCols; ++colNr)
 		{
 			SColumnDescription colDesc = m_stmt.DescribeColumn(colNr);
@@ -315,36 +340,75 @@ namespace exodbcexec
 			// add +3 chars to charsize: 1 for '\0', 1 for '.' and 1 for '-':
 #ifdef _WIN32
 			WCharColumnBufferPtr pColBuffer = WCharColumnBuffer::Create(colDesc.m_charSize + 3, colDesc.m_name, colDesc.m_sqlType, ColumnFlag::CF_SELECT);
+#else
+			CharColumnBufferPtr pColBuffer = CharColumnBufferPtr::Create(colDesc.m_charSize + 3, colDesc.m_name, colDesc.m_sqlType, ColumnFlag::CF_SELECT);
 #endif
-			StringColumnWrapper wrapper(pColBuffer);
-			stringCols.push_back(wrapper);
 			m_stmt.BindColumn(pColBuffer, colNr);
+			StringColumnWrapper wrapper(pColBuffer);
+			m_currentColumns.push_back(wrapper);
 		}
+	}
 
-		// and print current or all
+
+	void ExodbcExec::UnbindColumns()
+	{
+		LOG_DEBUG(boost::str(boost::format(u8"Unbinding %d columns") % m_currentColumns.size()));
+		m_stmt.UnbindColumns();
+		m_currentColumns.clear();
+	}
+
+
+	void ExodbcExec::Select(SelectMode mode)
+	{
+		switch (mode)
+		{
+		case SelectMode::First:
+			m_stmt.SelectFirst();
+			break;
+		case SelectMode::Last:
+			m_stmt.SelectLast();
+			break;
+		case SelectMode::Next:
+			m_stmt.SelectNext();
+			break;
+		case SelectMode::Prev:
+			m_stmt.SelectPrev();
+			break;
+		}
+	}
+
+
+	void ExodbcExec::Print(ExodbcExec::PrintMode mode)
+	{
+		// print current or all
 		if (mode == PrintMode::Current)
 		{
-			PrintCurrentRecord(stringCols);
+			LOG_INFO(CurrentRecordToString(m_currentColumns));
 		}
 		else if (mode == PrintMode::All)
 		{
 			size_t rowCount = 1;
-			bool haveNext = m_stmt.SelectNext();
-			while (m_stmt.SelectNext())
+
+			bool haveNext = false;
+			if (m_stmt.IsForwardOnlyCursor())
+				haveNext = m_stmt.SelectNext();
+			else
+				haveNext = m_stmt.SelectFirst();
+			while (haveNext)
 			{
 				stringstream ss;
 				ss << u8"row: " << rowCount << ": ";
-				ss << PrintCurrentRecord(stringCols);
+				ss << CurrentRecordToString(m_currentColumns);
 				LOG_INFO(ss.str());
+				haveNext = m_stmt.SelectNext();
 				++rowCount;
 			}
 			LOG_INFO(u8"No more results available");
 		}
-		m_stmt.UnbindColumns();
 	}
 	
 
-	std::string ExodbcExec::PrintCurrentRecord(const std::vector<exodbc::StringColumnWrapper>& columns) const
+	std::string ExodbcExec::CurrentRecordToString(const std::vector<exodbc::StringColumnWrapper>& columns) const
 	{
 		stringstream ss;
 		std::vector<StringColumnWrapper>::const_iterator it = columns.begin();
@@ -366,11 +430,22 @@ namespace exodbcexec
 	{
 		LOG_INFO(u8"Any input that is not recognized as a command will be executed as SQL against");
 		LOG_INFO(u8"the database connected to.");
-		LOG_INFO(u8"Commands can be abbreviated using the first letters of the command, like");
-		LOG_INFO(u8"command documented as '!(e)xit' can be invoked using '!e' or '!exit'.");
+		LOG_INFO(u8"Commands can be abbreviated. For example, 'Exit SQL execution documented as'");
+		LOG_INFO(u8"'!exit,!e,!q', can be invoked using '!exit' or '!e' or '!q'.");
 		LOG_INFO(u8"Commands are:");
-		LOG_INFO(u8" !(e)xit    Exit SQL execution.");
-		LOG_INFO(u8" !(p)rint   Print all records of the current recordset.");
-		LOG_INFO(u8" !(h)elp    Show this help.");
+		LOG_INFO(u8" !exit,!e,!q         Exit SQL execution.");
+		LOG_INFO(u8" !quit               See !exit.");
+		LOG_INFO(u8" !next,!sn           Select next record.");
+		LOG_INFO(u8" !prev,!sp           Select previous record.");
+		LOG_INFO(u8" !first,!sf          Select first record.");
+		LOG_INFO(u8" !last,!sl           Select last record.");
+		LOG_INFO(u8" !printAll,!pa       Print all records of the current recordset.");
+		LOG_INFO(u8"                     If forward only cursors is set to false, !printAll will");
+		LOG_INFO(u8"                     first execue a '!first' and then print and iterate all");
+		LOG_INFO(u8"                     records by calling '!next'.");
+		LOG_INFO(u8"                     If forward only cursors is set to true, all remaining");
+		LOG_INFO(u8"                     records found using '!next' are printed");
+		LOG_INFO(u8" !printCurrent,!pc   Print the current record.");
+		LOG_INFO(u8" !(h)elp             Show this help.");
 	}
 }
