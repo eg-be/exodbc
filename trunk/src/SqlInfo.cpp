d2ec26b3e15f107bc001ffa8558e29683ac90fb6
eg <eg@7898c5a7-19f2-4356-953d-35c5c242a1bf>
2017-04-12 19:58:07 +0000

    move values from SqlGetInfo into SqlInfo object

---
+++ trunk/src/SqlInfo.cpp
@@ -0,0 +1,47 @@
+﻿/*!
+* \file SqlInfo.cpp
+* \author Elias Gerber <eg@elisium.ch>
+* \date 12.04.2017
+* \brief Source file for SqlInfo.
+* \copyright GNU Lesser General Public License Version 3
+*
+*/
+
+// Own header
+#include "SqlInfo.h"
+
+// Same component headers
+// Other headers
+// Debug
+#include "DebugNew.h"
+
+// Static consts
+// -------------
+
+using namespace std;
+
+namespace exodbc
+{
+	// Construction
+	// ------------
+
+
+	// Destructor
+	// -----------
+
+
+	// Implementation
+	// --------------
+	SqlInfo::ValueType SqlInfo::GetValueType() const noexcept
+	{
+		switch (m_value.which())
+		{
+		case 0:
+			return ValueType::USmallInt;
+		case 1:
+			return ValueType::UInt;
+		default:
+			return ValueType::String;
+		}
+	}
+}
