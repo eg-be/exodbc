3c43512821c5cd50c2ea288a692683913588df52
eg <eg@7898c5a7-19f2-4356-953d-35c5c242a1bf>
2017-04-14 22:31:36 +0000

    #274 work in progress

---
+++ trunk/include/exodbc/SqlInfoProperty.h
@@ -0,0 +1,126 @@
+﻿/*!
+* \file SqlInfoProperty.h
+* \author Elias Gerber <eg@elisium.ch>
+* \date 12.04.2017
+* \brief Header file for the SqlInfo class.
+* \copyright GNU Lesser General Public License Version 3
+*/
+
+#pragma once
+
+// Same component headers
+#include "exOdbc.h"
+#include "SqlHandle.h"
+
+// Other headers
+#include <boost/variant.hpp>
+
+// System headers
+#include <map>
+#include <string>
+#include <set>
+
+// Forward declarations
+// --------------------
+
+namespace exodbc
+{
+	// Consts
+	// ------
+
+	// Structs
+	// -------
+
+	// Classes
+	// -------
+	class EXODBCAPI SqlInfoProperty
+	{
+	public:
+		enum class InfoType
+		{
+			Driver,
+			DBMS,
+			DataSource,
+			SupportedSql,
+			SqlLimits,
+			ScalarFunction,
+			Conversion
+		};
+
+		enum class ValueType
+		{
+			USmallInt,
+			UInt,
+			String_N_Y,
+			String_Any
+		};
+
+		typedef boost::variant<
+			SQLUSMALLINT,
+			SQLUINTEGER,
+			std::string
+		> Value;
+
+		SqlInfoProperty(SQLUSMALLINT infoId, const std::string& InfoName, InfoType infoType, ValueType valueType);
+
+		ValueType GetValueType() const noexcept { return m_valueType; };
+		InfoType GetInfoType() const noexcept { return m_infoType; };
+		Value GetValue() const noexcept { return m_value; };
+		SQLUSMALLINT GetInfoId() const noexcept { return m_infoId; };
+		std::string GetName() const noexcept { return m_infoName; };
+		bool GetValueRead() const noexcept { return m_valueRead; };
+
+		void ReadProperty(ConstSqlDbcHandlePtr pHdbc);
+		std::string GetStringValue() const;
+
+	private:
+		void ReadInfoValue(ConstSqlDbcHandlePtr pHDbc, SQLUSMALLINT fInfoType, std::string& sValue) const;
+		void ReadInfoValue(ConstSqlDbcHandlePtr pHDbc, SQLUSMALLINT fInfoType, SQLPOINTER rgbInfoValue, SQLSMALLINT cbInfoValueMax, SQLSMALLINT* pcbInfoValue) const;
+
+		SQLUSMALLINT m_infoId;
+		std::string m_infoName;
+		InfoType m_infoType;
+		ValueType m_valueType;
+		Value m_value;
+		bool m_valueRead;
+	};
+
+
+	class EXODBCAPI SqlInfoPropertyNameVisitor
+		: public boost::static_visitor<std::string>
+	{
+	public:
+		std::string operator()(SQLUSMALLINT ui) const;
+		std::string operator()(SQLUINTEGER ui) const;
+		std::string operator()(const std::string& ui) const;
+	};
+
+
+	class EXODBCAPI SqlInfoProperties
+	{
+	public:
+		SqlInfoProperties()
+		{};
+
+		SqlInfoProperties(ConstSqlDbcHandlePtr pHdbc);
+
+		void Init(ConstSqlDbcHandlePtr pHdbc);
+
+		struct SLexicalCompare {
+			bool operator() (const SqlInfoProperty& lhs, const SqlInfoProperty& rhs) const {
+				return lhs.GetName() < rhs.GetName();
+			}
+		};
+		std::set<SqlInfoProperty, SLexicalCompare> GetProperties(SqlInfoProperty::InfoType infoType) const noexcept;
+
+	private:
+		void LoadProperties(ConstSqlDbcHandlePtr pHdbc);
+		void RegisterDbmsProperties();
+		void RegisterDataSourceProperties();
+
+		void RegisterProperty(SQLUSMALLINT id, const std::string& name, SqlInfoProperty::InfoType infoType, SqlInfoProperty::ValueType valueType);
+
+		typedef std::map<SQLUSMALLINT, SqlInfoProperty> PropsMap;
+		PropsMap m_props;
+	};
+} // namespace exodbc
