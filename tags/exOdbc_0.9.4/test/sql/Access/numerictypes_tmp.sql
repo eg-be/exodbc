36f2ea3e5ea7f82d1180d4ed49dc4ad080fa2770
eg <eg@7898c5a7-19f2-4356-953d-35c5c242a1bf>
2017-05-08 18:31:21 +0000

    tagged too early, version info was wrong

--- tags/exOdbc_0.9.4/test/sql/Access/numerictypes_tmp.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE numerictypes_tmp;
-
-CREATE TABLE numerictypes_tmp (
-		idnumerictypes Long PRIMARY KEY, 
-		tdecimal_18_0 DECIMAL(18 , 0), 
-		tdecimal_18_10 DECIMAL(18 , 10), 
-		tdecimal_5_3 DECIMAL(5 , 3)
-	);
--- tags/exOdbc_0.9.4/test/sql/Access/unicodeTable_tmp.sql
+++
@@ -1,6 +0,0 @@
-DROP TABLE unicodetable_tmp;
-
-CREATE TABLE unicodetable_tmp (
-		idunicodetable Long PRIMARY KEY,
-		content VARCHAR(255)
-	);
