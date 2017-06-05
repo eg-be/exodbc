40a1c87bec6f7cf714669c926d516a84bb8a09c1
eg <eg@7898c5a7-19f2-4356-953d-35c5c242a1bf>
2017-06-05 14:57:05 +0000

    Create integertypes for PostgreSQL

---
+++ trunk/test/sql/PostgreSQL/integertypes_tmp.sql
@@ -0,0 +1,10 @@
+DROP TABLE exodbc.integertypes_tmp;
+
+CREATE TABLE exodbc.integertypes_tmp (
+		idintegertypes integer NOT NULL, 
+		tsmallint smallint, 
+		tint integer, 
+		tbigint bigint,
+    PRIMARY KEY(idintegertypes)
+	);
+	
