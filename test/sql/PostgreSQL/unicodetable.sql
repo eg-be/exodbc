89a9a95c7074123257d63f8918720082987e0fcd
eg <23149233+eg-be@users.noreply.github.com>
2017-06-05 16:25:52 +0000

    add unicodetable

---
+++ test/sql/PostgreSQL/unicodetable.sql
@@ -0,0 +1,13 @@
+DROP TABLE exodbc.unicodetable;
+
+CREATE TABLE exodbc.unicodetable (
+		idunicodetable integer NOT NULL, 
+		content varchar(255),
+    PRIMARY KEY(idunicodetable)
+	);
+		
+INSERT INTO exodbc.unicodetable (idunicodetable, content) VALUES (1, 'После смерти отца оставил учёбу и поступил на службу газетным репортёром');
+INSERT INTO exodbc.unicodetable (idunicodetable, content) VALUES (2, '因此他对世界的原型的认识就比较广泛。迁徙和旅行对他的创作很有助益');
+INSERT INTO exodbc.unicodetable (idunicodetable, content) VALUES (3, 'Zürih üniversitesin de başladığı Alman Filolojisi eğitimini 1933 deki babasının ölümü üzerine');
+INSERT INTO exodbc.unicodetable (idunicodetable, content) VALUES (4, 'מקס פריש נולד ב-15 במאי 1911 בציריך, בן לאדריכל פרנץ ברונו פריש ולאשתו קרולינה בטינה');
+INSERT INTO exodbc.unicodetable (idunicodetable, content) VALUES (5, 'Στο έργο του ο Φρις διεισδύει, σχολιάζει και αναλύει τα διλήμματα του σύγχρονου ανθρώπου:');
